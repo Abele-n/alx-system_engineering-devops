aa02b934b857985c7627435354fd80e0f3b24cd6
Abele-n <otienoabele89@gmail.com>
2023-12-22 09:24:47 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-#this installs puppet-lint -v 2.5.0
+#pip install python
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package {'python3-pip':
+  ensure => installed,
+}
+
+exec {'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
