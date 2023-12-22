8339598315e69126797a98b0dc73cae5b01bd5bc
Abele-n <otienoabele89@gmail.com>
2023-12-22 09:33:04 -0800

    task 1 updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
-#pip install python
+#installs the package puppet-lint
 
-package {'python3-pip':
-  ensure => installed,
-}
-
-exec {'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  require => Package['python3-pip'],
+package {'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
