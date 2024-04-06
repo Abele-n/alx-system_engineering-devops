4997467a6c010b92fc7903db7c9c0e7ded3709ef
Abele-n <otienoabele89@gmail.com>
2024-04-06 02:59:57 -0700

    task 1, api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,16 @@
+"""
+a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url="users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url="todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for m in todos if m.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(completed), len(todos)))
+    [print("\t {}".format(h)) for h in completed]
