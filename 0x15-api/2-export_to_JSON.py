dd6254f3167762e3ded53dedd67113e4995d1218
Abele-n <otienoabele89@gmail.com>
2024-04-07 23:02:04 -0700

    task2, export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format."""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w", newline="") as jsonfile:
+        json.dump({user_id: [{
+            "task": m.get("title"),
+            "completed": m.get("completed"),
+            "username": username
+        } for m in todos]}, jsonfile)
